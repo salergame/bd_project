d69eb68f39e20f258a4422a72ac53d400aca44fb
Sanzhar <salergame07@gmail.com>
2023-12-14 11:05:03 +0600

    second

--- main.py
+++ main.py
@@ -7,7 +7,12 @@ conn=psycopg2.connect(host='localhost',
                       password='1234567')
 cur=conn.cursor()
 
-conn.commit()
+cur.execute('SELECT Password FROM Accounts')
+all_users = cur.fetchall()
+gg = []
+for i in all_users:
+  gg.append(i[0])
+
 TOKEN = '6392060028:AAEVRZLwG3yJk2hNoxPR5MiNQNpofxRhaRM'
 
 bot = telebot.TeleBot(TOKEN)
@@ -21,10 +26,16 @@ def start(message):
     btn1 = types.KeyboardButton("Поздороваться")
     btn2 = types.KeyboardButton("Задать вопрос")
     btn3=types.KeyboardButton("Зарегестрироваться")
-    markup.add(btn1, btn2,btn3)
+    btn4=types.KeyboardButton("Вход")
+    markup.add(btn1, btn2,btn3,btn4)
     bot.send_message(message.chat.id, text="Привет", reply_markup=markup)
 def welcome(message):
-    bot.send_message(message.chat.id, text="Добро пожаловать")
+    if message.text in gg:
+      bot.send_message(message.chat.id, text="Добро пожаловать")
+    else:
+      bot.send_message(message.chat.id, text="Пароль введен не правильно")
+
+
 @bot.message_handler(content_types=['text'])
 def func(message):
     if(message.text == "Поздороваться"):
@@ -39,6 +50,11 @@ def func(message):
     elif(message.text=="Зарегестрироваться"):
       bot.send_message(message.chat.id, text="Введите свои пароль: ")
       bot.register_next_step_handler(message, welcome )
+    elif(message.text=="Вход"):
+      markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+      bot.send_message(message.chat.id, text="Введите свои пароль: ")
+      bot.register_next_step_handler(message, welcome )
+      
     elif(message.text == "Как тебя зовут?"):
         bot.send_message(message.chat.id, "Меня зовут Марат")
     
@@ -50,11 +66,12 @@ def func(message):
         button1 = types.KeyboardButton("Поздороваться")
         button2 = types.KeyboardButton("Задать вопрос")
         button3 = types.KeyboardButton("Зарегестрироваться")
-        markup.add(button1, button2,button3)
+        button4= types.KeyboardButton("Вход")
+        markup.add(button1, button2,button3,button4)
         bot.send_message(message.chat.id, text="Вы вернулись в главное меню", reply_markup=markup)
     else:
         bot.send_message(message.chat.id, text="На такую комманду я не запрограммировал..")
 
-
+conn.commit()
 if __name__ == '__main__':
-    bot.polling(none_stop=True)
+    bot.infinity_polling()
